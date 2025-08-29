7ecf7ffcca0a760a27051bfadcb400f67a94a934
aliourhzal <aourhzal@student.1337.ma>
2025-08-29 12:38:07 +0100

    email verification page

---
+++ frontend/app/verify/page.tsx
@@ -0,0 +1,62 @@
+'use client';
+
+import { useEffect, useState } from "react";
+import { useSearchParams } from "next/navigation";
+import axios from "axios";
+
+import UnprotectedPageWrapper from "@/components/ui/UnprotectedPageWrapper";
+import { Success } from "@/components/VerifyEmail/Success";
+import { Failed } from "@/components/VerifyEmail/Failed";
+import { Expired } from "@/components/VerifyEmail/Expired";
+import { Loading } from "@/components/VerifyEmail/Loading";
+
+export default function VerifyPage() {
+  const searchParams = useSearchParams();
+  const [loading, setLoading] = useState(true);
+  const [verificationStatus, setVerificationStatus] = useState<'success' | 'failed' | 'expired' | null>(null);
+  const token = searchParams.get("token");
+
+  const verifyEmail = async (token: string) => {
+    try {
+      await axios.get(`http://localhost:8081/api/v1/auth/email-verification?token=${token}`);
+
+      setVerificationStatus('success');
+    } catch (e) {
+      if (axios.isAxiosError(e) && e.response) {
+        if (e.status === 400) {
+          setVerificationStatus('expired');
+        } else {
+          setVerificationStatus('failed');
+        }
+      }
+      console.log(e);
+    } finally {
+      setLoading(false);
+    }
+  }
+
+  useEffect(() => {
+    if (token) {
+      verifyEmail(token);
+    }
+  }, [token]);
+
+  return (
+    <UnprotectedPageWrapper>
+      <div className="flex items-center justify-center grow">
+        {
+          !loading && verificationStatus === 'success' && <Success />
+        }
+        {
+          !loading && verificationStatus === 'failed' && <Failed />
+        }
+        {
+          !loading && verificationStatus === 'expired' && <Expired />
+        }
+        {
+          loading && <Loading />
+        }
+      </div>
+    </UnprotectedPageWrapper>
+  );
+}
