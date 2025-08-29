b95e7afa3dc008d423c17eb8450071082cda7707
aliourhzal <aourhzal@student.1337.ma>
2025-08-29 17:34:55 +0100

    add msg prop to pass the appropriate msg

--- frontend/components/VerifyEmail/Success.tsx
+++ frontend/components/VerifyEmail/Success.tsx
@@ -2,14 +2,14 @@ import { CgCheckO } from "react-icons/cg";
 import Link from "next/link";
 import { StatusContainer } from "./StatusContainer";
 
-export function Success() {
+export function Success({msg}: {msg: string}) {
   return (
     <StatusContainer>
       <div className="bg-emerald-500 w-[60px] aspect-square flex items-center justify-center rounded-full absolute left-1/2 top-[-30px] translate-x-[-50%] ">
         <CgCheckO className="text-white text-4xl" />
       </div>
       <h1 className="text-3xl font-medium">Email Verification</h1>
-      <p>Your email has been verified successfully</p>
+      <p>{msg}</p>
       <Link href="/login" className="bg-emerald-500 py-2 px-8 rounded-full text-white font-medium text-lg">Login</Link>
     </StatusContainer>
   );
