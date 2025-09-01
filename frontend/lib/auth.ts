dac4d67a0b816231c0f01c3a1699a227d368d1c3
aliourhzal <aourhzal@student.1337.ma>
2025-09-01 15:05:30 +0100

    console log token

--- frontend/lib/auth.ts
+++ frontend/lib/auth.ts
@@ -4,8 +4,5 @@ import { NextRequest } from "next/server";
 export async function getUser(req: NextRequest) {
   const token = req.cookies.get("token")?.value;
 
-  if (!token) return null;
-  try {
-    jwtVerify(token, )
-  }
+  console.log("Token from cookies:", token);
 }
